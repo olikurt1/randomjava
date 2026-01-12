62c5fe9463544cf5bc167ad0f60d1e2e921a6fa9
olikurt1 <145839522+olikurt1@users.noreply.github.com>
2026-01-12 19:00:11 +0000

    Chaning input format to be taken in as one line and then broken into substrings and finally parsed into integers where needed

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Line 11: fixes logical issue where the operator input takes in an empty line
 ----> the first input takes in number\n automatically through the buffer. nextInt() only takes in number with \n left over.
 \n is a newline command which means that the operator input "absorbs" this and then program goes straight to second number input.
 --> The fix means that the \n is taken in to the buffer before the operator input commences.
+
+CHANGING INPUT FORMAT TO SINGLE LINE
+When breaking into substrings, an end index is optional for correct syntax but it is required if there are further values otherwise the substring will start at the beginning index and continue for the rest of the string.
--- calculator.java
+++ calculator.java
@@ -4,19 +4,18 @@ public class calculator {
         Scanner input = new Scanner(System.in);
         System.out.println("-------------BEGIN-------------");
        
-        /*First Number */
+        
         System.out.print("");
-        int firstNumber = input.nextInt();
+        String calculation = input.nextLine();
+        /*Have to have an end index if there are further numbers/texts */
+        String strNum1 = calculation.substring(0,1);
+        String operator = calculation.substring(1,2);
+        String strNum2 = calculation.substring(2);
 
-        /*flushes the previous input */
-        input.nextLine();
+        
 
-        System.out.print("");
-        String operator = input.nextLine();
-
-        /*Second number */
-        System.out.print("");
-        int secondNumber = input.nextInt();
+        int firstNumber = Integer.parseInt(strNum1);
+        int secondNumber = Integer.parseInt(strNum2);
 
         switch(operator){
             case "+":
@@ -33,6 +32,9 @@ public class calculator {
 
             case "/":
                 System.out.println(firstNumber / secondNumber);
+                break;
+            default:
+                System.out.println("Incorrect Input");
                 break;
         }
     }
