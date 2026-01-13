160ea45e5bc41fdafccbce2838da2ba245439d86
olikurt1 <145839522+olikurt1@users.noreply.github.com>
2026-01-13 17:02:30 +0000

    Adding rounds to the game

--- RPS.java
+++ RPS.java
@@ -3,54 +3,82 @@ import java.util.Random;
 public class RPS {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        int userScore = 0;
+        int computerScore = 0;
 
-        String[] computerChoices = {"rock", "paper", "scissors"};
-        Random random =  new Random();
-        int randomIndex = random.nextInt(3);
+        System.out.println("How many rounds would you like to play: ");
+        int rounds = input.nextInt();
 
-        String computerChoice = computerChoices[randomIndex];
-        System.out.print("Enter your choice, (r)ock, (p)aper, (s)cissors: ");
-        String userChoice = input.nextLine();     
+        for(int round = 0; round < rounds; round++){
+            String[] computerChoices = {"rock", "paper", "scissors"};
+            Random random =  new Random();
+            int randomIndex = random.nextInt(3);
+
+            String computerChoice = computerChoices[randomIndex];
+            System.out.print("Enter your choice, (r)ock, (p)aper, (s)cissors: ");
+            String userChoice = input.nextLine();     
+            input.nextLine();
         
-        if(userChoice == "r"){
-            switch(computerChoice){
-                case "rock":
-                    System.out.println("Its a draw");
-                    
-                case "paper":
-                    System.out.println("Computer wins");
-                    break;
-                case "scissors":
-                    System.out.println("User wins");
-                    break;
+            if(userChoice == "r"){
+                switch(computerChoice){
+                    case "rock":
+                        System.out.println("Its a draw");
+                        break;
+                    case "paper":
+                        System.out.println("Computer wins");
+                        computerScore += 1;
+                        break;
+                    case "scissors":
+                        System.out.println("User wins");
+                        userScore += 1;
+                        break;
+                }
             }
-        }
-        else if(userChoice == "s"){
-            switch(computerChoice){
-                case "scissors":
-                    System.out.println("Its a draw");
-                    break;
-                case "paper":
-                    System.out.println("User wins");
-                    break;
-                case "rock":
-                    System.out.println("Computer wins");
-                    break;
+            else if(userChoice == "s"){
+                switch(computerChoice){
+                    case "scissors":
+                        System.out.println("Its a draw");
+                        break;
+                    case "paper":
+                        System.out.println("User wins");
+                        userScore += 1;
+                        break;
+                    case "rock":
+                        System.out.println("Computer wins");
+                        computerScore += 1;
+                        break;
+                }
             }
-        }
-        else if(userChoice == "p"){
-            switch(computerChoice){
-                case "paper":
-                    System.out.println("Its a draw");
-                    break;
-                case "scissors":
-                    System.out.println("Computer wins");
-                    break;   
-                case "rock":
-                    System.out.println("User wins");
-                    break;
+            else if(userChoice == "p"){
+                switch(computerChoice){
+                    case "paper":
+                        System.out.println("Its a draw");
+                        break;
+                    case "scissors":
+                        System.out.println("Computer wins");
+                        computerScore += 1;
+                        break;
+                           
+                    case "rock":
+                        System.out.println("User wins");
+                        userScore += 1;
+                        break;
+                }
             }
+            System.out.println("Computers choice was: "+computerChoice);
         }
-        System.out.println("Computers choice was: "+computerChoice);
+        System.out.println("User score was: "+ userScore);
+        System.out.println("Computer score was: "+ computerScore);
+        if(userScore > computerScore){
+            System.out.println("User wins game!");
+        }
+        else if(computerScore > userScore){
+            System.out.println("Computer wins game!");
+        }
+        else{
+            System.out.println("DRAW!!");
+        }
+
+        
     }
 }
