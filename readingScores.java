b3e22a0faec52359e9350c5f9a397d46a1526ae1
olikurt1 <145839522+olikurt1@users.noreply.github.com>
2026-01-20 16:29:38 +0000

    Adding a file that will read the saved score file back

---
+++ readingScores.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.*;
+public class readingScores {
+    public static void main(String[] args)throws Exception{
+        Scanner file = new Scanner(Path.of("ScoreSave.txt"));
+        //checking for a line below current line
+        while(file.hasNext()){
+            //assign the current line to a variable
+            String line = file.nextLine();
+            //print the variable
+            System.out.println(line);
+        }
+    }
+}
